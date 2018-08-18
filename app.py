e528306c22d3253b31876d4f66ab3b9db6bcab12
Samy Cherfaoui <scherfaoui@berkeley.edu>
2018-08-17 21:18:50 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from tempmail import Throwaway
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
+user = Throwaway()
+email = user.get_email_address()
+#email = "test"
 
 @app.route("/")
 def index():
-    user = Throwaway()
-	email = user.getEmailAddress()
+    return """<form method="POST"><p>Your email is {email}</p><input type="submit" value="Check email" /></form>""".format(email=email)
+
+@app.route('/', methods=['POST'])
+def form_data():
+	emails = user.get_mailbox(email)
+	#emails = ["hi", "bye"]
+	return """<p>Your emails are: {test}</p><form method="POST"><input type="submit" value="Recheck" /></form>""".format(test=emails)
+
 
